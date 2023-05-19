bdd0705047df8c9b9dc4eefc78a4903b43e471f7
zinu <doglife222@gmail.com>
2023-05-20 02:34:14 +0900

    feat:회원가입기능

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    styledComponents: true
+  },
+  async rewrites() {
+    return {
+        fallback: [
+            {
+                source: '/:path*',
+                destination: 'https://ec322d8e-041e-4c0d-afd9-d4cfebab274a.mock.pstmn.io/:path*',
+            },
+        ],
+    };
+},
 }
 
 module.exports = nextConfig
